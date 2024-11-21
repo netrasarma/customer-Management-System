2eeb068639a2b9f0b577ca149ec9dcdf82f30db6
netrasarma <133686911+netrasarma@users.noreply.github.com>
2024-11-21 20:43:46 +0530

    Update main.py
    updated page path

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import streamlit as st
 # ---------- PAGE SETUP -------------
 
 home_page=st.Page(
-    page="Pages/home.py",
+    page="home.py",
     title="Home",
     icon=":material/home:",
     default=True,
 )
 employee=st.Page(
-    page="Pages/Employee_Portal.py",
+    page="Employee_Portal.py",
     title="Employee Portal",
     icon=":material/manage_accounts:",
 )
 admin=st.Page(
-    page="Pages/Admin_Portal.py",
+    page="Admin_Portal.py",
     title="Admin Portal",
     icon=":material/admin_panel_settings:",
 )
 contact=st.Page(
-    page="Pages/contact.py",
+    page="contact.py",
     title="Contact",
     icon=":material/contact_phone:",
 )
 about=st.Page(
-    page="Pages/About.py",
+    page="About.py",
     title="About",
     icon=":material/groups:",
 )
